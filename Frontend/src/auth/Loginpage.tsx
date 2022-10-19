a43eee66038ca6e2ee2d54bcf3e708d98d3f382f
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-19 12:07:41 +0700

    set

---
+++ Frontend/src/auth/Loginpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Loginpage() {
+  return <div>Loginpage</div>;
+}
+
+export default Loginpage;
---
+++ Frontend/src/pages/Homepages.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Homepages() {
+  return <div>Homepages</div>;
+}
+
+export default Homepages;
