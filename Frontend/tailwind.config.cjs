62fc538fa00f60fc99ab40e8d45a8b80dfd73611
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-19 17:52:10 +0700

    update navbar

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require('flowbite/plugin')
+  ],
 };
