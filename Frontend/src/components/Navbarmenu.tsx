98d1c2508642842a6a4a2487582a3684ab4cccac
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-20 13:32:14 +0700

    edit

--- Frontend/src/components/Navbarmenu.tsx
+++ Frontend/src/components/Navbarmenu.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Navbar, Button } from "flowbite-react";
 
 export default function Navbarmenu() {
   return (
-    <Navbar fluid={true} rounded={true}>
+    <Navbar fluid={true}>
       <Navbar.Brand href="https://flowbite.com/">
         <img
           src="https://flowbite.com/docs/images/logo.svg"
@@ -14,11 +14,6 @@ export default function Navbarmenu() {
           Flowbite
         </span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
-        <Button>Get started</Button>
-        <Navbar.Toggle />
-      </div>
       <Navbar.Collapse>
         <Navbar.Link href="/navbars" active={true}>
           Home
@@ -28,6 +23,15 @@ export default function Navbarmenu() {
         <Navbar.Link href="/navbars">Pricing</Navbar.Link>
         <Navbar.Link href="/navbars">Contact</Navbar.Link>
       </Navbar.Collapse>
+      <div className="flex flex-wrap items-center gap-2">
+        <div>
+          <Button>Login</Button>
+        </div>
+        <div>
+          <Button>Register</Button>
+        </div>
+      </div>
+      <Navbar.Toggle />
     </Navbar>
   );
 }
